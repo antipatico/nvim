3883e36dfbbf3daeaab5b268c5b143324b0e350d
antipatico <antipatico@users.noreply.github.com>
2025-03-25 13:41:15 +0100

    feat: more remaps

--- lua/antipatico/lazy/telescope.lua
+++ lua/antipatico/lazy/telescope.lua
@@ -10,5 +10,21 @@ return {
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
     -- Git files: find files within the current git repo
     vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+    -- Project search: search string within project
+    vim.keymap.set("n", "<leader>ps", function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    -- Project search word: search word at cursor
+    vim.keymap.set("n", "<leader>pws", function()
+      local word = vim.fn.expand('<cword>')
+      builtin.grep_string({ search = word })
+    end)
+    -- Project search big word: big word expands points, underscores and as such
+    vim.keymap.set("n", "<leader>pWs", function()
+      local word = vim.fn.expand('<cWORD>')
+      builtin.grep_string({ search = word })
+    end)
+    -- Vim help: serach through internal Neovim help system
+    vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
   end
 }
--- lua/antipatico/remap.lua
+++ lua/antipatico/remap.lua
@@ -1,12 +1,21 @@
 -- Project view: go back to netrw (file explorer)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Use <leader>cd to change directory tu current Netrw folder
+-- ================ Netrw custom keymaps ================
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "netrw",
   callback = function()
+    -- Change directory: change directory tu current Netrw folder
     vim.keymap.set("n", "<leader>cd", function()
       vim.cmd("execute 'cd' b:netrw_curdir")
     end, { buffer = true, desc = "Change working directory to Netrw current directory" })
+    -- Navigate downwards
+    vim.keymap.set('n', 'l', '<CR>', { remap = true, buffer = true })
+    -- Navigate upwards
+    vim.keymap.set('n', 'h', '-^', { remap = true, buffer = true })
+    -- Navigate back
+    vim.keymap.set('n', 'H', 'u', { remap = true, buffer = true })
   end
 })
+-- ================ Netrw custom keymaps end ================
+
