c7ab9263050e11ee675514a73055fbea4be2736b
antipatico <antipatico@users.noreply.github.com>
2025-03-25 22:02:33 +0100

    refactor: more ideomatic lazy.nvim key binds

--- lua/antipatico/lazy/telescope.lua
+++ lua/antipatico/lazy/telescope.lua
@@ -2,29 +2,25 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    -- Load the plugins and, most importantly, set the key binds
-    require("telescope").setup({})
+  opts = {},
+  keys = (function()
     local builtin = require("telescope.builtin")
-    -- Project files: find files within the current project
-    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-    -- Git files: find files within the current git repo
-    vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-    -- Project search: search string within project
-    vim.keymap.set("n", "<leader>ps", function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
-    -- Project search word: search word at cursor
-    vim.keymap.set("n", "<leader>pws", function()
-      local word = vim.fn.expand('<cword>')
-      builtin.grep_string({ search = word })
-    end)
-    -- Project search big word: big word expands points, underscores and as such
-    vim.keymap.set("n", "<leader>pWs", function()
-      local word = vim.fn.expand('<cWORD>')
-      builtin.grep_string({ search = word })
-    end)
-    -- Vim help: serach through internal Neovim help system
-    vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-  end
+    return {
+      { "<leader>pf", builtin.find_files, desc = "Find Project Files" },
+      { "<C-p>", builtin.git_files, desc = "Find Git Files" },
+      { "<leader>ps", function() 
+          builtin.grep_string({ search = vim.fn.input("Grep > ") }) 
+        end, desc = "Search in Project" 
+      },
+      { "<leader>pws", function() 
+          builtin.grep_string({ search = vim.fn.expand('<cword>') }) 
+        end, desc = "Search Word at Cursor" 
+      },
+      { "<leader>pWs", function() 
+          builtin.grep_string({ search = vim.fn.expand('<cWORD>') }) 
+        end, desc = "Search Big Word at Cursor" 
+      },
+      { "<leader>vh", builtin.help_tags, desc = "Search Vim Help" }
+    }
+  end)()
 }
--- lua/antipatico/set.lua
+++ lua/antipatico/set.lua
@@ -1,2 +1,32 @@
 -- Define the leader as space
 vim.g.mapleader = " "
+-- Always use fat cursor
+vim.opt.guicursor = ""
+-- Show line numbers
+vim.opt.nu = true
+-- Show relative line numbers
+vim.opt.relativenumber = true
+-- Default tab config using
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+-- Use spaces instead of tab chars
+vim.opt.expandtab = true
+-- Enable indenting
+vim.opt.smartindent = true
+-- Disable line wrapping
+vim.opt.wrap = false
+-- Disable swap files and backup files
+vim.swapfile = false
+vim.backup = false
+-- Enable undodir for huge undofile
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+vim.opt.undofile = true
+-- Enable pretty colors
+vim.opt.termguicolors = true
+-- Have 8 lines up and down the cursor (if not at the end or start of the file)
+vim.opt.scrolloff = 8
+-- Fast update time
+vim.opt.updatetime = 50
+-- Show the color column
+vim.opt.colorcolumn = "80"
