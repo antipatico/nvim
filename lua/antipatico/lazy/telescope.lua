d912fa6749cab13ab350330c491a3962c6433016
jscannella <jacopo.scannella@sony.com>
2025-04-02 14:25:20 +0200

    fix: telescope first install

--- lua/antipatico/lazy/telescope.lua
+++ lua/antipatico/lazy/telescope.lua
@@ -2,25 +2,22 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
-  keys = (function()
+  config = function()
+    require('telescope').setup({})
+    -- Set keys. Needs to use config because telescope.builtin
+    -- is not available in the keys step.
     local builtin = require("telescope.builtin")
-    return {
-      { "<leader>pf", builtin.find_files, desc = "Find Project Files" },
-      { "<C-p>", builtin.git_files, desc = "Find Git Files" },
-      { "<leader>ps", function() 
-          builtin.grep_string({ search = vim.fn.input("Grep > ") }) 
-        end, desc = "Search in Project" 
-      },
-      { "<leader>pws", function() 
-          builtin.grep_string({ search = vim.fn.expand('<cword>') }) 
-        end, desc = "Search Word at Cursor" 
-      },
-      { "<leader>pWs", function() 
-          builtin.grep_string({ search = vim.fn.expand('<cWORD>') }) 
-        end, desc = "Search Big Word at Cursor" 
-      },
-      { "<leader>vh", builtin.help_tags, desc = "Search Vim Help" }
-    }
-  end)()
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Project find: find files by name" })
+    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Find git files" })
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end, { desc = "Search string in files" })
+    vim.keymap.set('n', '<leader>pws', function()
+      builtin.grep_string({ search = vim.fn.expand("<cword>") })
+    end, { desc = "Search word at cursor in files" })
+    vim.keymap.set('n', '<leader>pWs', function()
+      builtin.grep_string({ search = vim.fn.expand("<cword>") })
+    end, { desc = "Search big word (extend some symbols as dot) at cursor in files" })
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Search vim help" }) 
+  end,
 }
