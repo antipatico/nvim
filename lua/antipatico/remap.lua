b66ec071903b14b1b7b49c8d303709f6cfa4887b
jscannella <jacopo.scannella@sony.com>
2025-04-02 11:48:28 +0200

    feat: more remaps

--- lua/antipatico/remap.lua
+++ lua/antipatico/remap.lua
@@ -1,21 +1,25 @@
--- Project view: go back to netrw (file explorer)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 -- ================ Netrw custom keymaps ================
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "netrw",
   callback = function()
-    -- Change directory: change directory tu current Netrw folder
     vim.keymap.set("n", "<leader>cd", function()
       vim.cmd("execute 'cd' b:netrw_curdir")
     end, { buffer = true, desc = "Change working directory to Netrw current directory" })
-    -- Navigate downwards
-    vim.keymap.set('n', 'l', '<CR>', { remap = true, buffer = true })
-    -- Navigate upwards
-    vim.keymap.set('n', 'h', '-^', { remap = true, buffer = true })
-    -- Navigate back
-    vim.keymap.set('n', 'H', 'u', { remap = true, buffer = true })
+    
+    vim.keymap.set('n', 'l', '<CR>', { remap = true, buffer = true, desc = "Navigate downwards" })
+    vim.keymap.set('n', 'h', '-^', { remap = true, buffer = true, desc = "Navigate upwards" })
+    vim.keymap.set('n', 'H', 'u', { remap = true, buffer = true, desc = "Navigate back" })
   end
 })
 -- ================ Netrw custom keymaps end ================
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Project view: go back to Netrw"})
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Copy the selected text to the system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy the entire line to the system clipboard" })
+vim.keymap.set("n", "<leader>pp", [["+p]], { desc = "Paste text from the system clipboard (after)" })
+vim.keymap.set({"n", "v"}, "<leader>pP", [["+P]], { desc = "Paste text from the system clipboard (before)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Keep the cursor to the center of the buffer when scrolling down"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Keep the cursor to the center of the buffer when scrolling up"})
+vim.keymap.set("n", "n", "nzzzv", { desc = "Keep the cursor to the center of the buffer when searching down" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Keep the cursor to the center of the buffer when seraching up" })
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode using CTRL+C" })
