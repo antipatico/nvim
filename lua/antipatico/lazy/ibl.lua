e5f9151d89d66c4b8379ebe185ec13241c7fb1b9
jscannella <jacopo.scannella@sony.com>
2025-04-10 16:25:28 +0200

    feat: ibl

---
+++ lua/antipatico/lazy/ibl.lua
@@ -0,0 +1,10 @@
+-- https://github.com/lukas=reineke/indent-blankline
+-- Show indent lines
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  tag = "v3.9.0",
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = { },
+}
