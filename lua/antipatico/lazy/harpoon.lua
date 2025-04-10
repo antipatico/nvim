666a006a265b7bf1d014f6d89fef0d4d19173eb9
jscannella <jacopo.scannella@sony.com>
2025-04-10 15:19:14 +0200

    harpoon: better key binds

--- lua/antipatico/lazy/harpoon.lua
+++ lua/antipatico/lazy/harpoon.lua
@@ -1,3 +1,7 @@
+-- https://github.com/ThePrimeagen/harpoon
+-- Harpoon is a simple plugin allowing to pin important files of a project
+-- to quickly switch between them.
+-- A project is defined by the CWD.
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
@@ -7,6 +11,8 @@ return {
     harpoon:setup()
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,
       { desc = "[HARPOON] Add file to list" })
+    vim.keymap.set("n", "<leader>c", function() harpoon:list():clear() end,
+      { desc = "[HARPOON] Clear file list" })
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = "[HARPOON] Open menu" })
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end,
@@ -17,5 +23,13 @@ return {
       { desc = "[HARPOON] Open third file" })
     vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end,
       { desc = "[HARPOON] Open fourth file" })
+    vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end,
+      { desc = "[HARPOON] Replace first file" })
+    vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(2) end,
+      { desc = "[HARPOON] Replace second file" })
+    vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(3) end,
+      { desc = "[HARPOON] Replace third file" })
+    vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():replace_at(4) end,
+      { desc = "[HARPOON] Replace fourth file" })
   end,
 }
