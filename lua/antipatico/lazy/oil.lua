bf8db7c6312fc295385c78bc3211ea523a21365c
jscannella <jacopo.scannella@sony.com>
2025-04-10 14:27:45 +0200

    oil: always show .. directory

--- lua/antipatico/lazy/oil.lua
+++ lua/antipatico/lazy/oil.lua
@@ -38,6 +38,11 @@ return {
     view_options = {
       show_hidden = true,
       
+      -- This function deifines what is considered a "hidden" file
+      is_hidden_file = function(name, bufnr)
+        return (name ~= ".." and (name:match("^%.") ~= nil))
+      end,
+
       -- This function defines what will never be shown, even when show_hidden
       -- is set
       is_always_hidden = function(name, bufnr)
