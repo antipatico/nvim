e7b00d3f3ad960f89abd82223c4a122cf76e32a3
jscannella <jacopo.scannella@sony.com>
2025-04-25 10:45:56 +0200

    fix: undotree

--- lua/antipatico/lazy/undotree.lua
+++ lua/antipatico/lazy/undotree.lua
@@ -2,7 +2,7 @@
 -- Undotree allows you to see the undo tree of your edits for each file!
 return {
   "mbbill/undotree",
-  opts = {},
+  config = function() end,
   keys = {
     { "<leader>u", vim.cmd.UndotreeToggle }
   }
